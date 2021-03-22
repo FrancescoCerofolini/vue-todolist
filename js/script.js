c2739f27a8231d5302feb617d9c4938a4aaf9f04
FrancescoCerofolini <f.cerofolini96@gmail.com>
2021-03-22 20:08:22 +0100

    Esercizio Completo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        newTask: '',
+        tasks: [
+            'Fare esercizi',
+            'Allenarsi',
+            'Mangiare pollo e broccoli',
+            'Aprire la piscina'
+        ]
+    },
+
+    methods: {
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = '';
+        },
+
+        removeTask(index) {
+            console.log(index);
+            this.lastDeleted = this.tasks.splice(index, 1);    
+        }
+
+    }
+});
+
+
+Vue.config.devtools = true
+
+
+
 // Descrizione: Rifare l'esercizio della to do list come fatto assieme in classe:
 // - stampare in pagina un item per ogni elemento contenuto in un array
 // - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
